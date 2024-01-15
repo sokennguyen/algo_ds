6eb81bf56abd85214d8573aa74369a0f61491ab9
soken <nsongkien@gmail.com>
2024-01-15 20:43:12 +0200

    bubble sort

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,14 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-
+    let hi = haystack.length 
+    let lo = 0 
+    do {
+        let m = Math.floor(lo + (hi - lo)/2)
+        let val = haystack[m]
+        if (val === needle)
+            return true
+        else if (val > needle)
+            hi = m; 
+        else lo = m + 1;
+    } while (lo < hi)
+    return false
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,17 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let jumpStep = Math.floor(Math.sqrt(breaks.length))
+    
+    let i = jumpStep
+    for (; i<breaks.length; i+=jumpStep) {
+        if (breaks[i])
+            break;
+    }
 
+    i -= jumpStep;
+
+    for (let j=0; j < jumpStep && i<breaks.length; ++j, ++i) {
+        if (breaks[i])
+            return i
+    }
+    return -1; 
 }
